c4d8632aaf31d44128d5a4e0a2627b1e921ad1d4
AlessandroDimaria <arasmas3@gmail.com>
2025-12-03 11:49:05 +0100

    UserEffect

---
+++ 03-12/UserEffect/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import UsersList from "./components/UsersList";
+function App() {
+  return <div>
+    <h1>Elenco Utenti</h1>
+    <UsersList></UsersList>
+  </div>;
+}
+
+export default App;
---
+++ 03-12/UserEffect/src/components/UsersList.jsx
@@ -0,0 +1,22 @@
+import {useState, useEffect} from "react";
+export default function UsersList(){
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+       fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.json())
+            .then((data) => setUsers(data))
+            .catch((err) => setError(err.message))
+            .finally(() => setLoading(false));
+    }, []);
+
+    return <div>
+        {loading && "Caricamento in corso..."}
+        {error && `Errore: ${error}`}
+        {!loading && !error && users.map(user => {
+            return <p key={user.id}>{user.name}</p>;
+        })}
+    </div>;
+}

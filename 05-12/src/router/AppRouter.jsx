f7e921a6e7abd92db7eab2b2779d6778e7ec1d1a
AlessandroDimaria <arasmas3@gmail.com>
2025-12-05 10:19:21 +0100

    route id  e notfound

--- 05-12/src/router/AppRouter.jsx
+++ 05-12/src/router/AppRouter.jsx
@@ -5,29 +5,22 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
+import User from "../components/User";
+import NotFound from "../components/NotFound";
 
 export default function AppRouter() {
   return (
     <Routes>
-
       {/* 🏠 Rotta principale ("/") */}
-      <Route
-	      path="/"
-        element={<Home />}
-      />
-
+      <Route path="/" element={<Home />} />
+      {/* ℹ️ Rotta "user id" */}
+      <Route path="/user/:id" element={<User />} />
       {/* ℹ️ Rotta "Chi Siamo" */}
-      <Route
-	      path="/about"
-        element={<About />}
-      />
-
+      <Route path="/about" element={<About />} />
       {/* 📞 Rotta "Contatti" */}
-      <Route
-	      path="/contact"
-        element={<Contact />}
-      />
-
+      <Route path="/contact" element={<Contact />} />
+      {/*Rotta per gestire il caso in cui non sia presente una rotta*/}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
